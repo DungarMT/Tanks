e135fad2bb80401e4f51cee16398cd67238fecbc
DungarMT <Mihail.Dungar.Tyurenkov@gmail.com>
2016-05-14 16:02:12 +0300

    Fix screen bug

--- game.cpp
+++ game.cpp
@@ -4,20 +4,21 @@ Game::Game()
 {
     //Create main game scene
     QGraphicsScene *game_screen = new QGraphicsScene(this);
-    game_screen->setSceneRect(0, 0, 800, 600);
 
     //Create player tank
     Player *player = new Player();
-    player->setRect(200, 5, 32, 32);
+    player->setRect(0, 0, 32, 32);
 
     game_screen->addItem(player);
     player->setFlag(QGraphicsItem::ItemIsFocusable);
     player->setFocus();
 
     QGraphicsView *screen = new QGraphicsView(game_screen);
-    screen->setFixedSize(600, 600);
     screen->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     screen->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     screen->show();
+    screen->setFixedSize(600, 600);
+    game_screen->setSceneRect(0, 0, 800, 600);
 
+    player->setPos(screen->width()/2, screen->height() - player->rect().height()-5);
 }
