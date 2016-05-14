bed4ae9c32335647a8604133e7e84b95419e2cac
DungarMT <Mihail.Dungar.Tyurenkov@gmail.com>
2016-05-14 15:47:12 +0300

    Add game class

--- game.cpp
+++ game.cpp
@@ -4,16 +4,20 @@ Game::Game()
 {
     //Create main game scene
     QGraphicsScene *game_screen = new QGraphicsScene(this);
+    game_screen->setSceneRect(0, 0, 800, 600);
 
     //Create player tank
     Player *player = new Player();
-    player->setRect(0, 0, 100, 100);
+    player->setRect(200, 5, 32, 32);
 
     game_screen->addItem(player);
-
+    player->setFlag(QGraphicsItem::ItemIsFocusable);
     player->setFocus();
 
     QGraphicsView *screen = new QGraphicsView(game_screen);
+    screen->setFixedSize(600, 600);
     screen->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    screen->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    screen->show();
 
 }
--- main.cpp
+++ main.cpp
@@ -1,49 +1,11 @@
 #include <QApplication>
-#include <QGraphicsScene>
-#include <QGraphicsView>
-#include "player.h"
-<<<<<<< HEAD
-
-=======
-#include "block.h"
+#include "game.h"
 //yay
->>>>>>> origin/master
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QGraphicsScene *scene = new QGraphicsScene();
-
-    Player *player = new Player();
-    player->setRect(0, 0, 100, 100);
-
-    scene->addItem(player);
-
-
-
-
-
-
-    player->setFlag(QGraphicsItem::ItemIsFocusable);
-    player->setFocus();
-
-    QGraphicsView *view = new QGraphicsView(scene);
-    view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    view->show();
-    view->setFixedSize(600,600);
-    scene->setSceneRect(0,0,800,600);
-
-
-    Block *test_block=new Block();
-    test_block->setRect(100,100,50,50);
-    scene->addItem(test_block);
-
-
-
-
-    player->setPos(view->width()/2, view->height() - player->rect().height());
-
+    Game game;
 
     return a.exec();
 }
